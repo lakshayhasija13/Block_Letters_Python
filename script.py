cb0d59ff51f2071e29c33a6949cace24f0084ed7
Lakshay Hasija <hasijalakshay9@gmail.com>
2025-08-15 03:33:45 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,14 @@
+# TASK 1 : Write your full name and some info about ur self
+
+# Solution : 
+# NAME : LAKSHAY HASIJA
+# INFO :  I AM A PYTHON DEV
+
+# TASK 2 : Print first Letter of ur name
+print("""
+         L
+         L
+         L
+         L L L L L """)
+
+# SIMILARLY PRACTICE MULTI LINE STRING PRINT STATEMENTS FOR ALL UR NAME LETTERS
